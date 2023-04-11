495eb9d7e1a3db3ef36129da1bfc0c4a9bada9ed
ЧУПАКАБРА <artem.vakorin.96@mail.ru>
2023-04-12 00:32:54 +0700

    Решение третьей задачи

--- 1.3/Program.cs
+++ 1.3/Program.cs
@@ -2,3 +2,16 @@
 //4 -> да
 //-3 -> нет
 //7 -> нет
+
+Console.WriteLine("ВВедите число - ");
+
+int num_1 = Convert.ToInt32(Console.ReadLine());
+
+if ( num_1 % 2 == 1)
+{
+    Console.WriteLine("Число " + num_1 + " является: Нечётным");
+}
+else
+{
+   Console.WriteLine("Число " + num_1 + " является: Чётным");
+}
